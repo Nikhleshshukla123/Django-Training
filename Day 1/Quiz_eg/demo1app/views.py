9bae6513a4dd9425c5ae32e69c314a5f0e5c18ac
nikhlesh <126241612+Nikhleshshukla123@users.noreply.github.com>
2024-08-30 09:40:57 +0530

    Add files via upload

--- Day 1/Quiz_eg/demo1app/views.py
+++ Day 1/Quiz_eg/demo1app/views.py
@@ -5,66 +5,79 @@ from django.http import HttpResponse
 from django.template import loader
 
 def testpaper(request):
-    que1 = "Who developed python"
-    a1 = "Dennis Ritchie"
-    b1 = "Nikhlesh Shukla"
-    c1 = "Mustafa Quasimali"
-    d1 = "Pankaj Kushwah"
+    # this code is for Template Tag
+    que = "Who developed python"
+    a = "Dennis Ritchie"
+    b = "Nikhlesh Shukla"
+    c = "Mustafa Quasimali"
+    d = "Pankaj Kushwah"
 
-    que2 = "Who developed python"
-    a2 = "Dennis Ritchie"
-    b2 = "Nikhlesh Shukla"
-    c2 = "Mustafa Quasimali"
-    d2 = "Pankaj Kushwah"
+# this code is for Template Variable
+    # que1 = "Who developed python"
+    # a1 = "Dennis Ritchie"
+    # b1 = "Nikhlesh Shukla"
+    # c1 = "Mustafa Quasimali"
+    # d1 = "Pankaj Kushwah"
 
-    que3 = "Who developed python"
-    a3 = "Dennis Ritchie"
-    b3 = "Nikhlesh Shukla"
-    c3 = "Mustafa Quasimali"
-    d3 = "Pankaj Kushwah"
+    # que2 = "Who developed python"
+    # a2 = "Dennis Ritchie"
+    # b2 = "Nikhlesh Shukla"
+    # c2 = "Mustafa Quasimali"
+    # d2 = "Pankaj Kushwah"
 
-    que4 = "Who developed python"
-    a4 = "Dennis Ritchie"
-    b4 = "Nikhlesh Shukla"
-    c4 = "Mustafa Quasimali"
-    d4 = "Pankaj Kushwah"
+    # que3 = "Who developed python"
+    # a3 = "Dennis Ritchie"
+    # b3 = "Nikhlesh Shukla"
+    # c3 = "Mustafa Quasimali"
+    # d3 = "Pankaj Kushwah"
 
-    que5 = "Who developed python"
-    a5 = "Dennis Ritchie"
-    b5 = "Nikhlesh Shukla"
-    c5 = "Mustafa Quasimali"
-    d5 = "Pankaj Kushwah"
+    # que4 = "Who developed python"
+    # a4 = "Dennis Ritchie"
+    # b4 = "Nikhlesh Shukla"
+    # c4 = "Mustafa Quasimali"
+    # d4 = "Pankaj Kushwah"
+
+    # que5 = "Who developed python"
+    # a5 = "Dennis Ritchie"
+    # b5 = "Nikhlesh Shukla"
+    # c5 = "Mustafa Quasimali"
+    # d5 = "Pankaj Kushwah"
 
     context = {
-        'que1' : que1,
-        'a1' : a1,
-        'b1' : b1,
-        'c1' : c1,
-        'd1' : d1,
+# this code is for Template Tag
+        'que':que,
+        'options':[a,b,c,d]
 
-        'que2' : que2,
-        'a2' : a2,
-        'b2' : b2,
-        'c2' : c2,
-        'd2' : d2,
+# this code is for Template Variable
+        # 'que1' : que1,
+        # 'a1' : a1,
+        # 'b1' : b1,
+        # 'c1' : c1,
+        # 'd1' : d1,
 
-        'que3' : que3,
-        'a3' : a3,
-        'b3' : b3,
-        'c3' : c3,
-        'd3' : d3,
+        # 'que2' : que2,
+        # 'a2' : a2,
+        # 'b2' : b2,
+        # 'c2' : c2,
+        # 'd2' : d2,
 
-        'que4' : que4,
-        'a4' : a4,
-        'b4' : b4,
-        'c4' : c4,
-        'd4' : d4,
+        # 'que3' : que3,
+        # 'a3' : a3,
+        # 'b3' : b3,
+        # 'c3' : c3,
+        # 'd3' : d3,
 
-        'que5' : que5,
-        'a5' : a5,
-        'b5' : b5,
-        'c5' : c5,
-        'd5' : d5
+        # 'que4' : que4,
+        # 'a4' : a4,
+        # 'b4' : b4,
+        # 'c4' : c4,
+        # 'd4' : d4,
+
+        # 'que5' : que5,
+        # 'a5' : a5,
+        # 'b5' : b5,
+        # 'c5' : c5,
+        # 'd5' : d5
     }
 
     teplate = loader.get_template("testpaper.html")
---
+++ Day 1/demo2/demo2app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+def about(request):
+    template=loader.get_template('about.html')
+    res=template.render()
+    return HttpResponse(res)
+
+def home(request):
+    template=loader.get_template('home.html')
+    res=template.render()
+    return HttpResponse(res)
